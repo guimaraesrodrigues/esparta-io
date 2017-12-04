0daa12628f870eb6712f6b7b0df2ac1c9c3a613a
Rodrigo <guimaraes_rodrigues@yahoo.com>
2017-12-04 09:19:08 -0200

    Distinct without ArrayList, fix #1

--- Distinct.java
+++ Distinct.java
@@ -13,30 +13,28 @@ public class Distinct {
     
     public int distinct(int[]array){        
         
-        List<Integer> values = new ArrayList<Integer>();
-        
-        for(int i = 0; i< array.length; i++){            
-            if(values.contains(array[i]))
-                continue;
-            else
-                values.add(array[i]);            
-        }    
-        
-        return values.size();
+        int counter = 0;
+          
+        if(array.length > 0){
+            counter ++;
+            for(int i = 1; i< array.length; i++){
+                for (int j = i-1; j >= 0; j--)
+                    if(array[i] == array[j])
+                        break;
+                    else if (j == 0)
+                        counter ++;                          
+            }                
+        }      
+        return counter;
     }
     
     public static void main(String[] args) {
                
         int[]array;
         
-        //array = new Random().ints(100000, -1000000, 1000000).toArray();
+        array = new Random().ints(100000, -1000000, 1000000).toArray();
         //array = new Random().ints(10, -20, 20).toArray();
-        array = new int[]{2,1,1,2,3,1};
-                
-        System.out.println("Number of distinct values: " + new Distinct().distinct(array));
-        
-        //System.out.print("Entre com o tamanho do vetor: ");
-        //tamanho = scan.nextInt();        
-        
+        //array = new int[]{2,1,1,2,3,1, 1, 2, 4, 9, 9, 9};                   
+        System.out.println("Number of distinct values: " + new Distinct().distinct(array));                
     }
 }
